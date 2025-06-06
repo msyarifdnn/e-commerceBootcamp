ca9719b1d5d501596bcaf26483d1cdeb03358737
msyarifdnn <ms01574910@gmail.com>
2025-06-06 21:08:04 +0700

    Task14 Bootcamp Batch 3

---
+++ task14/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductsController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/cart', [HomeController::class, 'cart']);
+
+Route::resource('products', ProductsController::class);
