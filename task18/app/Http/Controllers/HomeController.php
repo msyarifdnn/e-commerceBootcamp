0d1d1913b1fd53736393be35658475a1583feba9
msyarifdnn <ms01574910@gmail.com>
2025-06-14 20:17:39 +0700

    Task18 Bootcamp Batch 3

---
+++ task18/app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class HomeController extends Controller
+{
+    public function index() {
+        $products = Product::paginate(2);
+        return view('home', compact('products'));
+    }
+
+    public function cart() {
+        return view('cart');
+    }
+}
